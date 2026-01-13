a6323e6b54ffc10f9bf285e5547e7728ab08e48a
nurealammiaji <nurealam.miaji@gmail.com>
2026-01-13 13:05:11 +0600

    new components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Home from './pages/Home/Home'
+import Navbar from './components/Navbar/Navbar'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <Home />
+      <Footer />
     </>
   )
 }

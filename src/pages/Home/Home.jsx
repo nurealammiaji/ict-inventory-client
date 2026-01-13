248c9d4c4165a33b93b44beb11100818cd2e9a10
nurealammiaji <nurealam.miaji@gmail.com>
2026-01-14 01:03:46 +0600

    menu added to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { PiHandArrowDown, PiLaptop } from 'react-icons/pi'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div>
-      <p>This is Home Page</p>
+      <div className='flex items-center justify-center gap-10 mt-20'>
+        <Link to={"/loan-register"} className='border p-10 rounded-lg btn'>
+          <p className='flex items-center justify-center gap-5'><PiHandArrowDown className='text-3xl' /> <span className='text-xl'>Loan Register</span></p>
+        </Link>
+        <Link to={"/laptop-register"} className='border p-10 rounded-lg btn'>
+          <p className='flex items-center justify-center gap-5'><PiLaptop className='text-3xl' /><span className='text-xl'>Laptop Register</span></p>
+        </Link>
+      </div>
     </div>
   )
 }
